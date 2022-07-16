f58ca2ad84ccfbbf731490612fc2486e08cf2a15
Zyf21 <ivanovwfgkz@yandex.by>
2022-07-16 11:21:17 +0300

    Initial commit

---
+++ src/main/java/com/study/comtech/ComTechApplication.java
@@ -0,0 +1,13 @@
+package com.study.comtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComTechApplication.class, args);
+    }
+
+}
